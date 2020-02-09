ee8875ec46bf03db95503ec7888f301660be6acc
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-02-09 16:21:57 -0600

    Zoom: Reduce mouse sensitivity, refresh the renderer when exiting zoom

--- src/main/java/fi/dy/masa/tweakeroo/config/Callbacks.java
+++ src/main/java/fi/dy/masa/tweakeroo/config/Callbacks.java
@@ -32,6 +32,7 @@ import fi.dy.masa.tweakeroo.util.SnapAimMode;
 public class Callbacks
 {
     public static boolean skipWorldRendering;
+    private static double mouseSensitivity = -1.0;
 
     public static void init(MinecraftClient mc)
     {
@@ -89,7 +90,32 @@ public class Callbacks
         Hotkeys.SWAP_ELYTRA_CHESTPLATE.getKeybind().setCallback(callbackGeneric);
         Hotkeys.TOGGLE_GRAB_CURSOR.getKeybind().setCallback(callbackGeneric);
         Hotkeys.TOOL_PICK.getKeybind().setCallback(callbackGeneric);
-        Hotkeys.ZOOM_ACTIVATE.getKeybind().setCallback(callbackGeneric);
+        Hotkeys.ZOOM_ACTIVATE.getKeybind().setCallback((action, key) -> {
+            if (action == KeyAction.RELEASE)
+            {
+                // Refresh the rendered chunks when exiting zoom mode
+                mc.worldRenderer.scheduleTerrainUpdate();
+            }
+
+            if (key.getSettings().getActivateOn() == KeyAction.BOTH)
+            {
+                if (action == KeyAction.PRESS)
+                {
+                    // Only store it once
+                    if (mouseSensitivity == -1.0)
+                    {
+                        mouseSensitivity = mc.options.mouseSensitivity;
+                    }
+
+                    mc.options.mouseSensitivity = Math.min(mouseSensitivity, Configs.Generic.ZOOM_FOV.getDoubleValue() / 720.0);
+                }
+                else if (mouseSensitivity != -1.0)
+                {
+                    mc.options.mouseSensitivity = mouseSensitivity;
+                }
+            }
+            return false;
+        });
 
         Hotkeys.SKIP_ALL_RENDERING.getKeybind().setCallback(callbackMessage);
         Hotkeys.SKIP_WORLD_RENDERING.getKeybind().setCallback(callbackMessage);
