b7d2c875ce1aa47a40579f22f3be7accd2277aa4
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2021-08-30 08:16:33 -0500

    Disable Render Distance Fog: Increase the distance to a minimum of 512

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinBackgroundRenderer.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinBackgroundRenderer.java
@@ -5,11 +5,15 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.BackgroundRenderer;
+import net.minecraft.client.render.Camera;
 import fi.dy.masa.tweakeroo.config.Configs;
 import fi.dy.masa.tweakeroo.config.FeatureToggle;
 import fi.dy.masa.tweakeroo.renderer.RenderUtils;
 
-@Mixin(net.minecraft.client.render.BackgroundRenderer.class)
+@Mixin(BackgroundRenderer.class)
 public abstract class MixinBackgroundRenderer
 {
     @Inject(method = "applyFog(Lnet/minecraft/client/render/Camera;Lnet/minecraft/client/render/BackgroundRenderer$FogType;FZ)V", require = 0,
@@ -31,15 +35,15 @@ public abstract class MixinBackgroundRenderer
     @Inject(method = "applyFog(Lnet/minecraft/client/render/Camera;Lnet/minecraft/client/render/BackgroundRenderer$FogType;FZ)V", require = 0,
                at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;fogEnd(F)V", shift = At.Shift.AFTER))
     private static void disableRenderDistanceFog(
-            net.minecraft.client.render.Camera camera,
-            net.minecraft.client.render.BackgroundRenderer.FogType fogType,
+            Camera camera,
+            BackgroundRenderer.FogType fogType,
             float viewDistance, boolean thickFog, CallbackInfo ci)
     {
         if (Configs.Disable.DISABLE_RENDER_DISTANCE_FOG.getBooleanValue() && thickFog == false)
         {
-            float renderDistance = net.minecraft.client.MinecraftClient.getInstance().gameRenderer.getViewDistance();
-            com.mojang.blaze3d.systems.RenderSystem.fogStart(renderDistance * 1.6F);
-            com.mojang.blaze3d.systems.RenderSystem.fogEnd(renderDistance * 2.0F);
+            float distance = Math.max(512, MinecraftClient.getInstance().gameRenderer.getViewDistance());
+            RenderSystem.fogStart(distance * 1.6F);
+            RenderSystem.fogEnd(distance * 2.0F);
         }
     }
 }
