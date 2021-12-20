7763d04c78ebb98986730d10feb9812bf1e2ceb8
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2021-12-20 04:51:46 +0200

    Fix the Swap Almost Broken Tools feature not working when holding the button

--- src/main/java/fi/dy/masa/tweakeroo/tweaks/PlacementTweaks.java
+++ src/main/java/fi/dy/masa/tweakeroo/tweaks/PlacementTweaks.java
@@ -312,6 +312,8 @@ public class PlacementTweaks
             return ActionResult.PASS;
         }
 
+        InventoryUtils.trySwapCurrentToolIfNearlyBroken();
+
         ItemStack stackPre = player.getStackInHand(hand);
 
         if (Configs.Disable.DISABLE_AXE_STRIPPING.getBooleanValue() &&
