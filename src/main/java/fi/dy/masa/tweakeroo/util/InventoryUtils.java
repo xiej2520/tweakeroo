12a5a1f2720d2e86271917af4f1fc89a628a65bf
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2021-12-25 07:33:48 -0600

    Swap Almost Broken Tools: Fix swap to a dummy item with a full inventory

--- src/main/java/fi/dy/masa/tweakeroo/util/InventoryUtils.java
+++ src/main/java/fi/dy/masa/tweakeroo/util/InventoryUtils.java
@@ -388,7 +388,7 @@ public class InventoryUtils
 
     private static int findSuitableSlot(Container container, Predicate<ItemStack> itemTest)
     {
-        return findSuitableSlot(container, itemTest, new IntRange(0, container.slots.size() - 1));
+        return findSuitableSlot(container, itemTest, new IntRange(9, container.slots.size() - 1));
     }
 
     private static int findSuitableSlot(Container container, Predicate<ItemStack> itemTest, IntRange... ranges)
