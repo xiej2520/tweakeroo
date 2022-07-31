f546683549f8a3b508fff5fe8a099bc2470a87a1
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2022-07-31 14:14:03 -0500

    Fix/update the chat timestamps feature

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinChatHud.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinChatHud.java
@@ -4,14 +4,17 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.text.Text;
 import fi.dy.masa.tweakeroo.config.FeatureToggle;
 import fi.dy.masa.tweakeroo.util.MiscUtils;
 
-@Mixin(value = net.minecraft.client.gui.hud.ChatHud.class, priority = 1100)
+@Mixin(value = ChatHud.class, priority = 1100)
 public abstract class MixinChatHud extends net.minecraft.client.gui.DrawableHelper
 {
-    @ModifyVariable(method = "addMessage(Lnet/minecraft/text/Text;I)V", at = @At("HEAD"), argsOnly = true)
-    private net.minecraft.text.Text addTimestamp(net.minecraft.text.Text componentIn)
+    @ModifyVariable(method = "addMessage(Lnet/minecraft/text/Text;)V",
+                    at = @At("HEAD"), argsOnly = true)
+    private Text tweakeroo_addMessageTimestamp(Text componentIn)
     {
         if (FeatureToggle.TWEAK_CHAT_TIMESTAMP.getBooleanValue())
         {
