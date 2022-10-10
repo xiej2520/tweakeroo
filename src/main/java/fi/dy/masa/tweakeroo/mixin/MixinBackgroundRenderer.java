7c6299c0059d1f3af8098b1c6f57b30e4c5264af
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2022-10-10 13:04:11 -0500

    Also disable the horizon/fog darkening with "disableSkyDarkness"

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinBackgroundRenderer.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinBackgroundRenderer.java
@@ -4,6 +4,7 @@ import com.llamalad7.mixinextras.sugar.Local;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import com.mojang.blaze3d.systems.RenderSystem;
@@ -12,6 +13,7 @@ import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.tag.FluidTags;
+import net.minecraft.world.dimension.Dimension;
 import fi.dy.masa.tweakeroo.config.Configs;
 import fi.dy.masa.tweakeroo.config.FeatureToggle;
 import fi.dy.masa.tweakeroo.renderer.RenderUtils;
@@ -35,6 +37,14 @@ public abstract class MixinBackgroundRenderer
         {
             RenderUtils.overrideLavaFog(net.minecraft.client.MinecraftClient.getInstance().getCameraEntity());
         }
+    }
+
+    @Redirect(method = "render",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/world/dimension/Dimension;getHorizonShadingRatio()D"))
+    private static double tweakeroo_disableSkyDarkness(Dimension instance)
+    {
+        return Configs.Disable.DISABLE_SKY_DARKNESS.getBooleanValue() ? 1.0F : instance.getHorizonShadingRatio();
     }
 
     @Inject(method = "applyFog(Lnet/minecraft/client/render/Camera;Lnet/minecraft/client/render/BackgroundRenderer$FogType;FZ)V", require = 0,
