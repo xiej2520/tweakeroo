1f0bf2260c81a2228c398c663cbc087199fa0a8e
xiej2520 <jackyxie2520@outlook.com>
2025-06-26 20:32:44 -0500

    Add BreakReplace tweak

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinClientPlayerInteractionManager.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinClientPlayerInteractionManager.java
@@ -1,6 +1,18 @@
 package fi.dy.masa.tweakeroo.mixin;
 
 import fi.dy.masa.tweakeroo.config.Configs;
+import fi.dy.masa.tweakeroo.util.RayTraceUtils;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -33,6 +45,8 @@ public abstract class MixinClientPlayerInteractionManager
     @Shadow @Final private MinecraftClient client;
 
     @Shadow private int blockBreakingCooldown;
+
+    @Shadow public abstract ActionResult interactBlock(ClientPlayerEntity player, ClientWorld world, Hand hand, BlockHitResult hitResult);
 
     @Inject(method = "interactItem", at = @At(
             value = "INVOKE",
@@ -139,6 +153,24 @@ public abstract class MixinClientPlayerInteractionManager
         else
         {
             InventoryUtils.trySwapCurrentToolIfNearlyBroken();
+        }
+    }
+
+    @Inject(method = "breakBlock", at = @At("RETURN"))
+    private void handleBreakReplace(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        if (FeatureToggle.TWEAK_BREAK_REPLACE.getBooleanValue()) {
+            HitResult trace = this.client.crosshairTarget;
+            if (trace != null && trace.getType() == HitResult.Type.BLOCK) {
+                for (Hand hand : Hand.values()) {
+                    ItemStack stack = this.client.player.getStackInHand(hand);
+                    if (stack != null
+                        && Item.BLOCK_ITEMS.containsValue(stack.getItem())
+                        && this.interactBlock(this.client.player, this.client.world, hand, (BlockHitResult) trace).isAccepted()) {
+                        this.blockBreakingCooldown = 5;
+                        return;
+                    }
+                }
+            }
         }
     }
 
