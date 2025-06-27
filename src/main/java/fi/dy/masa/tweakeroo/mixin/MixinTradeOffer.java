cc12797ae2dacf147afdda160d8b93545589c3df
xiej2520 <jackyxie2520@outlook.com>
2025-06-26 20:32:44 -0500

    change preventTradeLocking to simply not increment uses

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinTradeOffer.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinTradeOffer.java
@@ -1,9 +1,6 @@
 package fi.dy.masa.tweakeroo.mixin;
 
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,16 +10,13 @@ import fi.dy.masa.tweakeroo.config.Configs;
 @Mixin(TradeOffer.class)
 public abstract class MixinTradeOffer
 {
-    @Shadow @Mutable @Final private int maxUses;
-
-    @Inject(method = "use", at = @At("RETURN"))
+    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
     private void preventTradeLocking(CallbackInfo ci)
     {
         if (Configs.Disable.DISABLE_VILLAGER_TRADE_LOCKING.getBooleanValue())
         {
-            // Prevents the trade from getting locked, by also incrementing
-            // the max uses every time the trade use count is incremented.
-            ++this.maxUses;
+            // Prevents the trade from getting locked, by not incrementing uses
+            ci.cancel();
         }
     }
 }
